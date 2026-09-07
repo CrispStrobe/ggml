3d60c3eb53be875cfd62938b8cb3ecb5aa81fc85
crispasr integration <crispasr-dev@localhost>
2026-09-07 11:12:54 +0000

    merge: retain SiGLU beside upstream clamp variant

--- include/ggml.h
+++ include/ggml.h
@@ -633,6 +633,7 @@ extern "C" {
         GGML_GLU_OP_SWIGLU_OAI,
         GGML_GLU_OP_GEGLU_ERF,
         GGML_GLU_OP_GEGLU_QUICK,
+        GGML_GLU_OP_SWIGLU_CLAMP,
         GGML_GLU_OP_SIGLU,
 
         GGML_GLU_OP_COUNT,
--- src/ggml.c
+++ src/ggml.c
@@ -1262,10 +1262,11 @@ static const char * GGML_GLU_OP_NAME[GGML_GLU_OP_COUNT] = {
     "SWIGLU_OAI",
     "GEGLU_ERF",
     "GEGLU_QUICK",
+    "SWIGLU_CLAMP",
     "SIGLU",
 };
 
-static_assert(GGML_GLU_OP_COUNT == 7, "GGML_GLU_OP_COUNT != 7");
+static_assert(GGML_GLU_OP_COUNT == 8, "GGML_GLU_OP_COUNT != 8");
 
 
 static_assert(sizeof(struct ggml_object)%GGML_MEM_ALIGN == 0, "ggml_object size must be a multiple of GGML_MEM_ALIGN");
